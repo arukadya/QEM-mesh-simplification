014dab9517898a8adb7c42d08e8a7d5272d6d748
Toshiki <toshiki.arukadya@outlook.jp>
2024-10-02 13:34:15 +0900

    Initial Commit

---
+++ QEMMeshSimplification/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  QEMMeshSimplification
+//
+//  Created by 須之内俊樹 on 2024/10/02.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
